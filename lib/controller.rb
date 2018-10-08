81d8915a952860820002cded98669335ed1f7bf6
Rafael Alonso <rafael.palonso@gmail.com>
2018-10-08 18:49:25 -0300

    Added scraping

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,5 +1,7 @@
 require_relative 'view'
 require_relative 'recipe'
+require 'nokogiri'
+require 'open-uri'
 
 class Controller
   def initialize(cookbook)
@@ -22,8 +24,46 @@ class Controller
   def destroy
     list
     unless @cookbook.all.empty?
-      indice = @view.ask_user_for_indice
+      indice = @view.ask_user_for_indice("remover")
       @cookbook.remove_recipe(indice)
     end
+  end
+
+  def scrape
+    recipe_array = []
+
+    # pega o ingrediente que o usuário quer importat
+    ingredient = @view.ask_user_for_ingredient
+
+    # url da página onde vamos procurar
+    url = "http://www.letscookfrench.com/recipes/find-recipe.aspx?aqt=#{ingredient}"
+
+    # Ler a página da url passada
+    html_file = open(url).read
+
+    # Transforma o html_file num objeto Nokofiri::HTML
+    # Serve para usarmos ferramentas como o .search
+    html_doc = Nokogiri::HTML(html_file)
+
+    # html_doc.search(parameters) << []
+    html_doc.search('.m_contenu_resultat').first(5).each do |element|
+      # A gente encontra o título no pedaço que obtivemos (element)
+      recipe_name = element.search('.m_titre_resultat a').text.strip
+
+      # A gente encontra a descrição no pedaço que obtivemos (element)
+      recipe_description = element.search('.m_texte_resultat').text.strip
+
+      # Populamos a array com uma nova Recipe, criada o name e desc
+      recipe_array.push(Recipe.new(recipe_name, recipe_description))
+    end
+
+    # Mostramos as receitas que conseguimos pro usuário
+    @view.listar(recipe_array)
+
+    # Pedimos por um índice
+    recipe_index = @view.ask_user_for_indice("importar")
+
+    # Salvamos a receita do índice fornecido
+    @cookbook.add_recipe(recipe_array[recipe_index])
   end
 end
--- lib/view.rb
+++ lib/view.rb
@@ -1,20 +1,22 @@
 class View
   def ask_user_for_name
     puts "Qual o nome da sua receita?"
-    name = gets.chomp
-    return name
+    gets.chomp
   end
 
   def ask_user_for_desc
     puts "Escreva a descrição da receita"
-    description = gets.chomp
-    return description
+    gets.chomp
   end
 
-  def ask_user_for_indice
-    puts "Qual o índice da receita que você quer remover?"
-    indice = gets.chomp.to_i - 1 # '3'.to_i ==> 3
-    return indice
+  def ask_user_for_indice(action)
+    puts "Qual o índice da receita que você quer #{action}?"
+    gets.chomp.to_i - 1 # '3'.to_i ==> 3
+  end
+
+  def ask_user_for_ingredient
+    puts "Qual o ingrediente que voce deseja usar?"
+    gets.chomp
   end
 
   def listar(recipes)
@@ -22,7 +24,8 @@ class View
       puts "Não há nenhuma receita salva ainda"
     else
       recipes.each_with_index do |recipe, indice|
-        puts "#{indice + 1} - #{recipe.name}: #{recipe.description}"
+        puts "#{indice + 1} - #{recipe.name}:"
+        puts "  #{recipe.description}"
       end
     end
   end
